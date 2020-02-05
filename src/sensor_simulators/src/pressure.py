104f7710174c6d701e5269a8aa1bee522e7dfc28
Carl Hildebrandt <carlhildebrandt@d-172-25-143-245.dhcp.virginia.edu>
2020-02-05 10:24:04 -0500

    Final update to pressure to allow

--- src/sensor_simulators/src/pressure.py
+++ src/sensor_simulators/src/pressure.py
@@ -22,7 +22,7 @@ class PressureSensor():
     self.altitude = 0
 
     # Used to estimate the pressure
-    self.pressure = Float64()
+    self.pressure = 0
 
     # Determines the baseline value of the perssure sensor at height 0m
     self.baseline_value = 0
@@ -43,23 +43,24 @@ class PressureSensor():
     # Set the rate of this loop
     rate = rospy.Rate(10)
 
-
+    # Message for sending data
+    pressure_msg = Float64()
 
     # While ROS is still running
     while not rospy.is_shutdown():
 
       # Publish the altitude
-      self.pressure_pub.publish(self.pressure)
+      self.pressure_pub.publish(pressure_msg)
 
       # Compute the pressure in milibars (according to https://www.weather.gov/media/epz/wxcalc/pressureAltitude.pdf)
       common_term = pow(4.43077*pow(10,6)-self.altitude, 0.2553026)
-      P = (-0.0000229797 * self.altitude * common_term) + (20.3635* common_term)
+      self.pressure = (-0.0000229797 * self.altitude * common_term) + (20.3635* common_term)
 
       # Add sensor noise
       noise = pow(10,-5) * random.uniform(-1, 1)
 
       # Set the pressure
-      self.pressure.data = P + noise - self.baseline_value
+      pressure_msg.data = self.pressure + noise - self.baseline_value
 
       # Sleep for the remainder of the loop
       rate.sleep()
